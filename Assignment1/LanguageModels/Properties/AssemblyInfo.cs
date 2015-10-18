e1642643ef3a2a877cb4a9b5104f2d72b15ee669
andresz <andresz@microsoft.com>
2015-10-17 18:29:41 -0700

    Add Normalizer class to read the file.

--- Assignment1/LanguageModels/Properties/AssemblyInfo.cs
+++ Assignment1/LanguageModels/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Assignment1")]
+[assembly: AssemblyTitle("UW.NLP.LanguageModels")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Assignment1")]
+[assembly: AssemblyProduct("LanguageModels")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("75a11deb-c721-46bc-8bdc-23906c9141bb")]
+[assembly: Guid("b557c676-0bac-4972-b39e-0bdc66e3639a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Assignment1/LanguageModels/SentenceNormalizer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace UW.NLP.LanguageModels
+{
+    public class SentenceNormalizer
+    {
+        public int N { get; private set; }
+
+        public string StartToken { get; private set; }
+
+        public string EndToken { get; private set; }
+
+        public string Separator { get; private set; }
+
+        public string PossibleEndToken { get; private set; }
+
+        public SentenceNormalizer(int n, string startToken, string endToken, string separator, string possibleEndToken)
+        {
+            if (n < 1) throw new ArgumentOutOfRangeException("n", "N for the N-gram must be greater than one.");
+            if (string.IsNullOrEmpty(startToken)) throw new ArgumentNullException("startToken");
+            if (string.IsNullOrEmpty(endToken)) throw new ArgumentNullException("endToken");
+            if (string.IsNullOrEmpty(separator)) throw new ArgumentNullException("separator");
+            if (string.IsNullOrEmpty(possibleEndToken)) throw new ArgumentNullException("possibleEndToken");
+
+            N = n;
+            StartToken = startToken;
+            EndToken = endToken;
+            Separator = separator;
+        }
+
+        public string Normalize(string sentence)
+        {
+            if (sentence == null) throw new ArgumentNullException("sentence");
+
+            StringBuilder normalizedSentence = new StringBuilder(GetNormalizedLength(sentence.Length));
+
+            normalizedSentence = AddStartTokens(normalizedSentence);
+
+            normalizedSentence = AddEndToken(sentence, normalizedSentence);
+
+            return normalizedSentence.ToString();
+        }
+
+        private int GetNormalizedLength(int originalLength)
+        {
+            return originalLength + ((N - 1) * (StartToken.Length + Separator.Length)) + Separator.Length + EndToken.Length;
+        }
+
+        private StringBuilder AddStartTokens(StringBuilder normalizedSentence)
+        {
+            for (int i = 0; i < N - 1; i++)
+            {
+                normalizedSentence.Insert(0, Separator);
+                normalizedSentence.Insert(0, StartToken);
+            }
+
+            return normalizedSentence;
+        }
+
+        private StringBuilder AddEndToken(string sentence, StringBuilder normalizedSentence)
+        {
+            if (sentence.EndsWith(PossibleEndToken))
+            {
+                normalizedSentence.Remove(normalizedSentence.Length - PossibleEndToken.Length, PossibleEndToken.Length);
+            }
+            normalizedSentence.Append(Separator);
+            normalizedSentence.Append(EndToken);
+
+            return normalizedSentence;
+        }
+    }
+}
